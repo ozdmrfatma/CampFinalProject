b0f46111a86b1762a53a79b70ff85bc92c33f6cd
ftma.ozdmr.13 <ftma.ozdmr.13@gmail.com>
2021-09-11 18:09:01 +0300

    Web Api katmanı kuruldu

---
+++ WebAPI/Startup.cs
@@ -0,0 +1,60 @@
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+
+        {
+            ///AUTOFAC, Ninject, CastleWindsor, StructureMap, LightInject, DryInject --->IoC Container
+            ///AOP -- .Net'in IoC Autofac'i enjekte edeceðiz
+            services.AddControllers();
+            services.AddSingleton<IProductService,ProductManager>();  ///Arka planda bir referans oluþtur.
+            services.AddSingleton<IProductDal, EfProductDal>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
