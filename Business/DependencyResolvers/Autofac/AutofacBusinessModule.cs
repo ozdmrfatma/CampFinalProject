b4c1619716ee9c6d80e58afe944e72adbdad3e5f
ftma.ozdmr.13 <ftma.ozdmr.13@gmail.com>
2021-09-13 21:45:38 +0300

    Autofac desteği eklendi

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)  //uygulama yayınlandığı zaman
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+        }
+    }
+}
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -33,8 +33,8 @@ namespace WebAPI
             ///AUTOFAC, Ninject, CastleWindsor, StructureMap, LightInject, DryInject --->IoC Container
             ///AOP -- .Net'in IoC Autofac'i enjekte edeceðiz
             services.AddControllers();
-            services.AddSingleton<IProductService,ProductManager>();  ///Arka planda bir referans oluþtur.
-            services.AddSingleton<IProductDal, EfProductDal>();
+            //services.AddSingleton<IProductService,ProductManager>();  ///Arka planda bir referans oluþtur.
+            //services.AddSingleton<IProductDal, EfProductDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
